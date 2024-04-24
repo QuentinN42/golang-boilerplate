eea80215ad053792dbb101ca83d380aa53dcb284
QuentinN42 <quentin@lieumont.fr>
2024-04-24 02:20:19 +0200

    feat: main
    Signed-off-by: QuentinN42 <quentin@lieumont.fr>

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -7,28 +7,15 @@ import (
 )
 
 type Envs struct {
-	MANDATORY_STR string
-	OPTIONAL_STR  *string
-
-	MANDATORY_INT int
-	OPTIONAL_INT  *int
-
-	OPTIONAL_BOOL1 bool // "" => false
-	OPTIONAL_BOOL2 *bool
+	OTEL_SERVICE_NAME string
 }
 
 func main() {
 	telemetry.Init()
-	logfmt.Info("Hello, World!")
 	envs, err := env.GetAll(&Envs{})
 	if err != nil {
-		logfmt.Fatal("Failed to get environment variables", err)
+		logfmt.Fatal("Unable to set up environment", err)
 	}
 
-	logfmt.Info("MANDATORY_STR: %v", envs.MANDATORY_STR)
-	logfmt.Info("OPTIONAL_STR: %v", *envs.OPTIONAL_STR)
-	logfmt.Info("MANDATORY_INT: %v", envs.MANDATORY_INT)
-	logfmt.Info("OPTIONAL_INT: %v", *envs.OPTIONAL_INT)
-	logfmt.Info("OPTIONAL_BOOL1: %v", envs.OPTIONAL_BOOL1)
-	logfmt.Info("OPTIONAL_BOOL2: %v", *envs.OPTIONAL_BOOL2)
+	logfmt.Trace("Env: %v", envs)
 }
